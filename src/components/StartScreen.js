548892a7384da8809b9c64a5005098adac4dccc2
Alina Frenchel <alina.frenchel@gmail.com>
2023-07-21 14:55:20 +0300

    React quiz project-useReducer practice

---
+++ src/components/StartScreen.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const StartScreen = ({ numQuestions, dispatch }) => {
+  return (
+    <div className="start">
+      <h3>Welcome to the React Quiz!</h3>
+      <p>
+        {" "}
+        <em>{numQuestions} questions to test your React mastery</em>
+      </p>
+      <button
+        className="btn btn-ui"
+        onClick={() => dispatch({ type: "start" })}
+      >
+        Let's start!
+      </button>
+    </div>
+  );
+};
+
+export default StartScreen;
